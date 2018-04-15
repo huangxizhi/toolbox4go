98814bf5dcbf0261098dcb77176381e4591414ec
huangtao <taohuang@emotibot.com>
2018-04-15 11:27:37 +0800

    modify test

--- redis_test.go
+++ redis_test.go
@@ -4,19 +4,28 @@ import (
 	"testing"
 )
 
+var (
+	test_ip string = "localhost"
+	test_port string = "6379"
+	test_db int  = 0
+)
+
+func getRedisClinet() *RedisClient {
+	return NewRedisClient(test_ip, test_port, test_db)
+}
+
 func TestSet(t *testing.T) {
 	key, val := "name", "redis"
-	_, err := GetRedisClient().Set(key, val)
+	_, err := getRedisClinet().Set(key, val)
 	if err != nil {
 		t.Errorf("set err:%v", err)
 	}
 
-	rVal, err := GetRedisClient().Get(key)
+	rVal, err := getRedisClinet().Get(key)
 	if err != nil {
 		t.Errorf("get err:%v", err)
 	}
 	if rVal != val {
 		t.Errorf("set and get not the save")
 	}
-
 }
